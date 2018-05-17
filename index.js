0e90a6d29e3ad2182db472de8f9d713c523b4837
JB desarrollo <ichiparanoid@gmail.com>
2018-05-16 23:03:43 -0500

    pasando un json de prueba

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-    res.send('Winter is coming!');
+// get an instance of router
+var router = express.Router();
+
+// home page route (http://localhost:8080)
+router.get('/', function(req, res) {
+    res.send('Winter is coming!!!');
 });
 
+// Experimento para pasar un json desde mi servidor nodejs
+router.get('/info', function(req, res) {
+    res.json({"nombre": "Jhon Barcasnegras",
+              "telef" : "3202794967",
+              "email" : "jhonbarc1978@outlook.com"});
+});
+
+// apply the routes to our application
+app.use('/', router);
 var port = process.env.PORT || 5000;
 
 app.listen(port, function () {
     console.log('Winter is coming!!! ' + port + '!');
 });
+
